369581d04d8ceba12e1fbebba0fc88c5bc6612f1
GuilhermeCampano <campanog@ryanair.com>
2019-07-28 01:35:14 +0100

    Display when a field is invalid

--- src/app/+enter/components/form-register/form-register.component.ts
+++ src/app/+enter/components/form-register/form-register.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Subscription, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 import { RegisterForm } from '@app/models';
-import { takeLast, take, throttle, throttleTime, takeUntil } from 'rxjs/operators';
+import { validateNotNull, validateUserName, validateUKPostCode, validateIrelandPostCode } from '@app/utils';
 
 @Component({
   selector: 'app-form-register',
@@ -14,6 +16,7 @@ export class FormRegisterComponent implements OnInit, OnDestroy {
   @Output() public movieChange = new EventEmitter<string>();
 
   public registerForm: FormGroup;
+  // @TODO add a constants for these options
   public readonly titleOptions = ['Mr', 'Mrs', 'Ms', 'Dr'];
   public readonly countryOptions = ['Ireland', 'United Kingdom'];
 
@@ -22,9 +25,11 @@ export class FormRegisterComponent implements OnInit, OnDestroy {
   constructor(private formBuilder: FormBuilder) {}
 
   public handleSubmit(): void {
-    if (this.registerForm.valid) {
-      this.formSubmit.emit(this.registerForm.value);
+    if (!this.registerForm.valid) {
+      this.registerForm.markAllAsTouched();
+      return;
     }
+    this.formSubmit.emit(this.registerForm.value);
   }
 
   public ngOnDestroy(): void {
@@ -35,22 +40,51 @@ export class FormRegisterComponent implements OnInit, OnDestroy {
   public ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       title: [this.titleOptions[0]],
-      firstName: ['', Validators.required],
-      lastName: [''],
-      username: [''],
+      firstName: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z]+$/)
+      ])],
+      lastName: ['', Validators.required],
+      username: ['', validateUserName],
       movie: [''],
-      country: [''],
+      country: ['', validateNotNull],
       postCode: [''],
     });
+
+    this.registerForm.get('firstName').valueChanges
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((firstName) => {
+        const lastNameControl = this.registerForm.get('lastName');
+        !!firstName ? lastNameControl.enable() : lastNameControl.disable();
+      });
+
+    this.registerForm.get('country').valueChanges
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((country) => {
+        const postCodeControl = this.registerForm.get('postCode');
+        if (country === 'Ireland') {
+          postCodeControl.setValidators(validateIrelandPostCode);
+        }
+        if (country === 'United Kingdom') {
+          postCodeControl.setValidators(validateUKPostCode);
+        }
+      });
+
     this.registerForm.get('movie').valueChanges
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe((movieValue) => {
         this.movieChange.emit(movieValue);
       });
+
   }
 
   public hasControlError(field: string): boolean {
     const control = this.registerForm.get(field);
     return control.invalid && (control.dirty || control.touched);
+  }
+
+  public get isLastNameDisabled(): boolean {
+    const {firstName} = this.registerForm.value;
+    return !firstName;
   }
 }
---
+++ src/app/utils/validators.utils.ts
@@ -0,0 +1,45 @@
+import { AbstractControl, ValidationErrors, Validators } from '@angular/forms';
+
+export const validateNotNull = (control: AbstractControl): ValidationErrors => {
+  if (!control.value || !control.value.length) {
+    return {notNull: true};
+  }
+
+  return null;
+};
+
+export const validateUserName = (control: AbstractControl): ValidationErrors => {
+  const { value } = control;
+  const isAnEmail = /@/;
+  const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (isAnEmail.test(value) && !emailRegex.test(value)) {
+    return {email: true};
+  }
+
+  if (value.length >= 1 && value.length < 5) {
+    return {minLength: true};
+  }
+
+  return null;
+};
+
+export const validateIrelandPostCode = (control: AbstractControl): ValidationErrors => {
+  const { value } = control;
+  if (!!value.length && (value.length < 6 || value.length > 10)) {
+    return {length: true};
+  }
+
+  return null;
+};
+
+export const validateUKPostCode = (control: AbstractControl): ValidationErrors => {
+  const { value } = control;
+  const ukPostCodeRegex = /^[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? [0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}$/;
+
+  if (!!value.length && !ukPostCodeRegex.test(value)) {
+    return {length: true};
+  }
+
+  return null;
+};
